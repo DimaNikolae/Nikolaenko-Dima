2440eff6ddc239b533b8875d4044f16cd0f4df55
dima nikolaenko <dimanikolaenko31@gmail.com>
2024-08-04 18:50:47 +0300

    fills

--- lessons_3.js
+++ lessons_3.js
@@ -1,4 +1,4 @@
-let number = 120
+let number = 15
 
 if (number % 3 === 0 && number % 5 === 0) {
     console.log("FizzBuzz"); 
@@ -8,7 +8,7 @@ if (number % 3 === 0 && number % 5 === 0) {
         console.log("Buzz")
 }
 
-let year = 2025
+let year = 2023
 
 if ((year % 4 === 0 && year % 100 !== 0) || (year % 400 === 0)) {
     console.log(year + " є високосним роком.")
@@ -16,3 +16,16 @@ if ((year % 4 === 0 && year % 100 !== 0) || (year % 400 === 0)) {
     console.log(year + " не є високосним роком.")
 }
 
+let years = 31
+
+if (years % 10 === 1 && years % 100 !== 11) {
+    console.log("Вам " + years + " рік");
+} else if (
+    years % 10 >= 2 &&
+    years % 10 <= 4 &&
+   !(years % 100 >= 12 && years % 100 <= 14)
+) {
+    console.log("Вам " + years + " роки");
+} else {
+    console.log("Вам " + years + " років");
+}
