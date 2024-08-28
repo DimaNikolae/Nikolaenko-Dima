48935656006d2c997d6b449b4efafdc5adc2979d
dima nikolaenko <dimanikolaenko31@gmail.com>
2024-08-28 13:58:31 +0300

    faile

--- lessons_6/lessons_6.js
+++ lessons_6/lessons_6.js
@@ -1,9 +1,54 @@
-function findGCD(a, b) {
-    // Використовуємо рекурсію для знаходження НСД
-    if (b === 0) {
-        return a;
+function doubleLetter(str) {
+    let result = '';
+    
+    for (let i = 0; i < str.length; i++) {
+        result += str[i] + str[i];
     }
-    return findGCD(b, a % b);
+    
+    return result;
 }
 
-console.log(findGCD(48, 18)); // Введіть числа
+console.log(doubleLetter("hello"));
+
+
+
+function padString(str, length, symbol, toLeft = false) {
+    // Перевіряємо, чи довжина рядка вже більша або дорівнює заданій довжині
+    if (str.length >= length) {
+        return str;
+    }
+
+    // Обчислюємо, скільки символів потрібно додати
+    const paddingLength = length - str.length;
+    const padding = symbol.repeat(paddingLength);
+
+    // Додаємо символи або зліва, або справа, залежно від прапора toLeft
+    return toLeft ? padding + str : str + padding;
+}
+
+// Виклик функції
+console.log(padString('Ivan', 7, '@', true));    
+console.log(padString('Ivan', 15, '@'));   
+
+
+
+function camelCase(str, separator) {
+    // Розділяємо рядок на слова за заданим роздільником
+    const words = str.split(separator);
+
+    // Перетворюємо перше слово в нижній регістр, а всі наступні слова в camelCase
+    const camelCased = words.map((word, index) => {
+        if (index === 0) {
+            return word.toLowerCase();
+        } else {
+            return word.charAt(0).toUpperCase() + word.slice(1).toLowerCase();
+        }
+    });
+
+    // Об'єднуємо всі слова в один рядок
+    return camelCased.join('');
+}
+
+console.log(camelCase('hello_world', '_'));  
+console.log(camelCase('make-javascript-great', '-')); 
+console.log(camelCase('convert this string', ' ')); 
